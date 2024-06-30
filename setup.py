03d95e73ea6d0018c637f665237eda3a66a48f9a
ppsingh <ppsingh.iitk@gmail.com>
2024-06-30 18:06:36 +0200

    update 2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setuptools.setup(
         description='Haystack based utils for NLP',
         author='Data Service Center GIZ',
         author_email='prashant.singh@giz.de',
-        package_dir={"": "nlp_utils"},
-        packages=setuptools.find_packages(where='nlp_utils'),  
+        package_dir={"": "nlputils"},
+        packages=setuptools.find_packages(where='nlputils'),  
          #external packages as dependencies
 )
