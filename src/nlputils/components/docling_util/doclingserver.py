8bed1b57f69223d1ff0702a91ca3beefbe8a6d9e
ppsingh <ppsingh.iitk@gmail.com>
2025-02-16 05:24:00 +0100

    update docling component

--- src/nlputils/components/docling_util/doclingserver.py
+++ src/nlputils/components/docling_util/doclingserver.py
@@ -1,13 +1,20 @@
 from docling.datamodel.base_models import ConversionStatus
 from docling.datamodel.document import ConversionResult
 from docling.datamodel.settings import settings
-from docling.document_converter import DocumentConverter
+from docling.document_converter import DocumentConverter, PdfFormatOption
+from docling.datamodel.pipeline_options import (
+    AcceleratorDevice,
+    AcceleratorOptions,
+    PdfPipelineOptions,
+)
+from docling.datamodel.base_models import InputFormat
 import logging
 import os
 import json
 import pandas as pd
 from pathlib import Path
 from typing import Iterable
+import yaml
 
 def send_doc(file_path):
     """ this is to process single file and get the doclingDocument"""
@@ -23,12 +30,11 @@ def send_doc(file_path):
 
     return result, filename
 
-
 def get_tables(doclingDoc, folder_location, filename):
-    save_to_folder = folder_location + "/" + filename + "/tables/" 
+    #doc_filename = doclingDoc.input.file.stem
+    save_to_folder = Path(folder_location + filename + "/tables")
     try:
-        if not os.path.exists():
-            os.makedirs(save_to_folder)
+        save_to_folder.mkdir(parents=True, exist_ok=True)
     except Exception as e:
         logging.warning(e)
     
@@ -37,50 +43,60 @@ def get_tables(doclingDoc, folder_location, filename):
         table_df: pd.DataFrame = table.export_to_dataframe()
 
         # Save the table as csv
-        element_csv_filename = save_to_folder + f"{table_ix+1}.csv"
+        element_csv_filename = save_to_folder / f"{table_ix+1}.csv"
         table_df.to_csv(element_csv_filename)
 
         # Save the table as html
-        element_html_filename = save_to_folder + f"{table_ix+1}.html"
+        element_html_filename = save_to_folder / f"{table_ix+1}.html"
         with element_html_filename.open("w") as fp:
             fp.write(table.export_to_html())
     return save_to_folder
 
-
 def save_output(doclingDoc, folder_location, filename):
-    save_to_folder = folder_location + "/" + filename + "/"
+    save_to_folder = Path(folder_location + filename)
     try:
-        if not os.path.exists(save_to_folder):
-            os.makedirs(save_to_folder)
+        save_to_folder.mkdir(parents=True, exist_ok=True)
     except Exception as e:
         logging.warning(e)
     
     tables_path = get_tables(doclingDoc=doclingDoc,folder_location=folder_location,
                              filename=filename)
-            
-    with (save_to_folder + f"{filename}.json").open("w", encoding="utf-8") as fp:
-        fp.write(json.dumps(doclingDoc.document.export_to_dict()))
-
+    try:
+        # Export Docling Json        
+        with (save_to_folder / f"{filename}.json").open("w", encoding="utf-8") as fp:
+            fp.write(json.dumps(doclingDoc.document.export_to_dict()))
+    except Exception as e:
+        logging.info(e)
+    
     # Export Text format:
-    with (save_to_folder + f"{filename}.txt").open("w", encoding="utf-8") as fp:
-        fp.write(doclingDoc.document.export_to_text())
+    try:
+        with (save_to_folder / f"{filename}.txt").open("w", encoding="utf-8") as fp:
+            fp.write(doclingDoc.document.export_to_text())
+    except Exception as e:
+        logging.info(e)
 
     # Export Markdown format:
-    with (save_to_folder + f"{filename}..md").open("w", encoding="utf-8") as fp:
-        fp.write(doclingDoc.document.export_to_markdown())
+    try:
+        with (save_to_folder / f"{filename}.md").open("w", encoding="utf-8") as fp:
+            fp.write(doclingDoc.document.export_to_markdown())
+    except Exception as e:
+        logging.info(e)
 
     # Export Document Tags format:
-    with (save_to_folder + f"{filename}.doctags").open("w", encoding="utf-8") as fp:
-        fp.write(doclingDoc.document.export_to_document_tokens())
+    try:
+        with (save_to_folder / f"{filename}.doctags").open("w", encoding="utf-8") as fp:
+            fp.write(doclingDoc.document.export_to_document_tokens())
+    except Exception as e:
+        logging.info(e)
+        
     
     return save_to_folder, tables_path
 
-
 def export_documents(
     conv_results: Iterable[ConversionResult],
-    output_dir:Path,
+    output_dir,
 ):
-    output_dir.mkdir(parents=True, exist_ok=True)
+    #output_dir.makedir(parents=True, exist_ok=True)
 
     success_count = 0
     failure_count = 0
@@ -90,15 +106,8 @@ def export_documents(
         if conv_res.status == ConversionStatus.SUCCESS:
             success_count += 1
             doc_filename = conv_res.input.file.stem
-            save_to_folder = output_dir + "/" + f"{doc_filename}/"
-            try:
-                if not os.path.exists(save_to_folder):
-                    os.makedirs(save_to_folder)
-            except Exception as e:
-                logging.warning(e)
 
-
-            save_output(doclingDoc=conv_res,folder_location=save_to_folder,
+            a,b = save_output(doclingDoc=conv_res,folder_location=output_dir,
                         filename=doc_filename)
 
         elif conv_res.status == ConversionStatus.PARTIAL_SUCCESS:
@@ -119,19 +128,31 @@ def export_documents(
     )
     return success_count, partial_success_count, failure_count
 
-
-def batch_processing(file_list):
+def batch_processing(file_list, output_dir, num_threads=8):
     logging.basicConfig(level=logging.INFO)
+    accelerator_options = AcceleratorOptions(
+        num_threads=num_threads, device=AcceleratorDevice.AUTO
+    )
+    pipeline_options = PdfPipelineOptions()
+    pipeline_options.accelerator_options = accelerator_options
+    pipeline_options.do_ocr = True
+    pipeline_options.do_table_structure = True
+    pipeline_options.table_structure_options.do_cell_matching = True
 
-    doc_converter = DocumentConverter()
+    doc_converter = DocumentConverter(
+        format_options={
+            InputFormat.PDF: PdfFormatOption(pipeline_options=pipeline_options)
+        }
+    )
 
     conv_results = doc_converter.convert_all(
         file_list,
         raises_on_error=False,  # to let conversion run through all and examine results at the end
     )
+ 
 
     success_count, partial_success_count, failure_count = export_documents(
-        conv_results, output_dir=Path("scratch")
+        conv_results, output_dir
     )
 
     if failure_count > 0:
@@ -139,5 +160,4 @@ def batch_processing(file_list):
             f"The example failed converting {failure_count} on {len(file_list)}."
         )
 
-
-
+    return success_count, partial_success_count, failure_count
